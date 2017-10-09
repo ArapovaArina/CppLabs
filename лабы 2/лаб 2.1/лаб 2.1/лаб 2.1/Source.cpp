d18ec57124c919f2f53423d544c598df7703ec33
Unknown <arina_arapova@mail.ru>
2017-10-09 23:03:59 +0500

    лаб 2.1

---
+++ лабы 2/лаб 2.1/лаб 2.1/лаб 2.1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+void();
+{
+	cin << n;
+	int a[n];
+
+	int sum = 0;
+	for (int i = 0; i<n; i++)
+		sum = a[i];
+	for (int i = 0; i<n; i++)
+		b[i] = (sum - a[i]) / (n - 1);
+	cout >> " " b;
+	return  0;
+}
