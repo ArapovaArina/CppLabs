4d1364c133afdf52f4b6d5410151ea3936d760fe
Unknown <arina_arapova@mail.ru>
2017-10-16 20:09:10 +0500

    такс

--- лабы 2/лаб 2.1/лаб 2.1/лаб 2.1/Source.cpp
+++ лабы 2/лаб 2.1/лаб 2.1/лаб 2.1/Source.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-#include <cmath>
-
+#include <math.h>
 using namespace std;
-void();
+int main()
 {
-	cin << n;
-	int a[n];
+	int n; 
+	cout << "¬ведите количество чисел: ";
+	cin >> n; 
 
+	int *a = new int[n]; 
+	
+	for (int i = 0; i < n; i++) 
+	{ cout << "¬ведите массив: \n";
+		cin >> a[i];  }
 	int sum = 0;
 	for (int i = 0; i<n; i++)
 		sum = a[i];
+
 	for (int i = 0; i<n; i++)
 		b[i] = (sum - a[i]) / (n - 1);
-	cout >> " " b;
-	return  0;
+	cout ї; " " b;
+	
+	return 0;
 }
+
+	
+	
---
+++ лабы 2/лаб 2.2/лаб 2.2/Source.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	cout << "¬ведите количество чисел в массиве: ";
+	int n;
+	cin >> n;
+	cout << "¬ведите массив: \n";
+	int *a = new int[n]; 
+	int max = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		if (a[i] > max)
+			max = a[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (abs(a[i]) == max)
+		{
+			a[i] = 1;
+		}
+		else
+		{
+			a[i] = 0;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+	return 0;
+}
