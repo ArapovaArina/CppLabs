7332cddf4eb4595deaf391e231daec094a079ab8
Unknown <arina_arapova@mail.ru>
2017-11-01 18:41:49 +0500

    лаб 2.2

---
+++ лабы 2/лаб 2.2/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	int n;
+	cout << "введите размер массива :";
+	cin >> n;
+	int *arr = new int[n];
+	int max = 0;
+	cout<<"введите число"<< endl; 
+	for (int i = 0; i < n; i++) {
+	cin >> arr[i];
+	if (max < arr[i]) 
+    max = arr[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (abs(arr[i]) != max) {
+			arr[i] = 0;
+		}
+		else {
+			arr[i] = 1;
+		};
+}
+	cout << "преобразованная строка ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+
+
+
+
+
+}
