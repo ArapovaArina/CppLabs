bbc4ecf46e385ae69ad006b237c8a73075d586c0
Unknown <arina_arapova@mail.ru>
2018-02-20 22:01:03 +0500

    new

--- SnakeTask/Source.cpp
+++ SnakeTask/Source.cpp
@@ -1,6 +1,7 @@
 ﻿#include<iostream>
 #include<conio.h> // to track keyboard input without closing
-
+#include <windows.h>
+#include <fstream>
 
 using namespace std;
 
@@ -8,6 +9,8 @@ bool gameOver;
 const int height = 20;
 const int width = 20;// ширина !!
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail = 0;
 enum motion {STOP = 0, LEFT, RIGHT, UP, DOWN};
 motion whereINeedToGo;
 
@@ -42,21 +45,32 @@ void Draw() {
 			if (i == y && j == x) // create our snake
 				cout << "$";
 			else if 
-				(i == fruitY && j == fruitY) // create our fruit
+				(i == fruitY && j == fruitX) // create our fruit
 				cout << "o";
 			else
-                cout << " ";
-				
-
+		       {
+					bool print = false;
+					 for (int k = 0; k < nTail; k++)
+				{
+					if (tailX[k] == j && tailY[k] == i)
+				{
+					cout << "o";
+					print = true;
+				}
+				}
+				if (!print)
+				 cout << " ";
+				}
 				if (j == width - 1)
 					cout << "*";
-		}
-		cout << endl; 
-	}
-
-	for (int i = 0; i < width + 2; i++)
-		cout << "*";
-	cout << endl;
+						}
+						cout << endl;
+					}
+				
+					for (int i = 0; i < width + 2; i++)
+						cout << "*";
+					cout << endl;
+	Sleep(150);
 
 
 }
@@ -88,6 +102,22 @@ void Input() {
 
 void Logic() {
 
+		int prevX = tailX[0];
+		int prevY = tailY[0];
+		int prev2X, prev2Y;
+		tailX[0] = x;
+		tailY[0] = y;
+		for (int i = 1; i < nTail; i++)
+		{
+			prev2X = tailX[i];
+			prev2Y = tailY[i];
+			tailX[i] = prevX;
+			tailY[i] = prevY;
+			prevX = prev2X;
+			prevY = prev2Y;
+		}
+
+
 	switch (whereINeedToGo)
 			{
 			case LEFT:
@@ -109,20 +139,42 @@ void Logic() {
 		gameOver = true;
 	if (x == fruitX && y == fruitY) {
 		score += 100;
-		fruitX = rand() % height;
-		fruitY = rand() % width;
+		fruitY = rand() % height;
+		fruitX = rand() % width; 
+		nTail++;
 
 	}
 
 }
 
-int main() {
-	Setup();
+void Results() {
 	char *name = new char[255];
 	cout << " enter your name ";
 	cin >> name;
 
+	ofstream writer;
+	writer.open("Results.txt", ios::out | ios::app);
+	writer << name << "    -   " << score << endl;
+	writer.close();
+}
+
+int main() {
+
+	fstream writer;
+	writer.open("Results.txt", ios::out | ios::trunc);
+	writer.close();
+	 
+	cout << "                    Shhhh! Hello, my name is Snake! " << endl << "                      Let`s play with me!" << endl << endl;
+	cout << "If you press : " << endl << " W - I`ll go UP" << endl << " S - I`ll go DOWN" << endl << " A - I`ll go LEFT" << endl << " D - I`ll go RIGHT" << endl << endl << endl;
+	cout << "So if you are ready to start press something ... " << endl << endl << "Good luck ! SHHHH!!!!!" << endl;
+	char buf ;
+	_getch();
+	
+    Setup();
+
+
 	char key = 'y';
+	
 	while (key != 'n')
 	{
 
@@ -133,15 +185,21 @@ int main() {
 			Logic();
 
 		}
-	  cout << "Add another student? [Y/n] ";
-	  cin.get(key);
-	  cin.ignore(1, '\n');
+
+	   Results();
+	 
+	   cout << "Again ? Press 'n' if uou don`t want to play ";
+	   key = _getch();
+
+	   Setup();
 	
 		 
 
 	}
 
-     
+	
+
+
 
 
 
